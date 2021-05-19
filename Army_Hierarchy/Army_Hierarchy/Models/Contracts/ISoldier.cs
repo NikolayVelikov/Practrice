710a334e8b457680413fd399c548ebe0eaff19be
NikolayVelikov <73721605+NikolayVelikov@users.noreply.github.com>
2021-05-19 23:49:54 +0300

    Creating interfaces
    Creating ISoldier and ISpy

---
+++ Army_Hierarchy/Army_Hierarchy/Models/Contracts/ISoldier.cs
@@ -0,0 +1,9 @@
+﻿namespace Army_Hierarchy.Models.Contracts
+{
+    public interface ISoldier
+    {
+        string Id { get; }
+        string FirstName { get; }
+        string LastName { get; }
+    }
+}
---
+++ Army_Hierarchy/Army_Hierarchy/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Army_Hierarchy
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
+            string b = Console.ReadLine();
+            double a = double.Parse(b);
+            Console.WriteLine(1+a);
+        }
+    }
+}
