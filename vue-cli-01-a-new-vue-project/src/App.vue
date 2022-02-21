499751c5d0b5d7cb4e0e341316a309e6f50c4548
Nikolay Velikov <73721605+NikolayVelikov@users.noreply.github.com>
2022-02-21 16:49:30 +0200

    Create vue task

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <h2>My friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from './components/FriendContact.vue';
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0123 90",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
