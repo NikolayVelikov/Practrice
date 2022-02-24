7012d768e507c97682728eae48dba4a090570e06
Nikolay Velikov <73721605+NikolayVelikov@users.noreply.github.com>
2022-02-24 19:49:11 +0200

    Create Vue practice task

--- vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,35 +1,60 @@
 <template>
   <li>
-    <h2>{{ friend.name }}</h2>
-    <button @click="toggleDetails">Show Details</button>
+    <h2>{{ name }} {{ friendIsFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleFavorite">
+      {{ friendIsFavorite ? "Toggle" : "Untoggle" }} Favorite
+    </button>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }}
+    </button>
     <ul v-if="detailsAreVisible">
-      <li><strong>Phone:</strong>{{ friend.phone }}</li>
-      <li><strong>Email:</strong>{{ friend.email }}</li>
+      <li><strong>Phone:</strong>{{ phoneNumber }}</li>
+      <li><strong>Email:</strong>{{ emailAddress }}</li>
     </ul>
   </li>
 </template>
 
 <script>
 export default {
+  //props: [ 'name', 'phoneNumber', 'emailAddress', "isFavorite" ],
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      //default: '0', // Could be function for more logic
+      //validator: function(value){
+      //  return value === '1' || value === '0';
+      //}
+    },
+  },
   data() {
     return {
       detailsAreVisible: false,
-      friend: {
-        id: "manuel",
-        name: "Manuel Lorenz",
-        phone: "0123 45678 90",
-        email: "manuel@localhost.com",
-      },
+      friendIsFavorite: this.isFavorite,
     };
   },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
+    toggleFavorite() {
+      this.friendIsFavorite = !this.friendIsFavorite;
+    },
   },
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
